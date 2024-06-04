a3bdc28b8001553e0638757f6e3511c1742fdd6f
Mikkel Mørk Hegnhøj <mikkel@fermyon.com>
2024-06-04 13:52:58 +0200

    Working code
    Signed-off-by: Mikkel Mørk Hegnhøj <mikkel@fermyon.com>

--- animal-facts-bot/src/index.ts
+++ animal-facts-bot/src/index.ts
@@ -1,9 +1,76 @@
 import { HandleRequest, HttpRequest, HttpResponse } from "@fermyon/spin-sdk"
 
-export const handleRequest: HandleRequest = async function (request: HttpRequest): Promise<HttpResponse> {
-  return {
-    status: 200,
-    headers: { "content-type": "text/plain" },
-    body: "Hello from TS-SDK"
-  }
+let decoder = new TextDecoder();
+
+type SlackCommand = {
+    token?: string | null;
+    team_id?: string | null;
+    team_domain?: string | null;
+    enterprise_id?: string | null;
+    enterprise_name?: string | null;
+    channel_id?: string | null;
+    channel_name?: string | null;
+    user_id?: string | null;
+    user_name?: string | null;
+    command?: string | null;
+    text?: string | null;
+    response_url?: string | null;
+    trigger_id?: string | null;
+    api_app_id?: string | null;
 }
+
+type AnimalFact = {
+    id: number;
+    name: string;
+    species: string;
+    family: string;
+    habitat: string;
+    place_of_founc: string;
+    diet: string;
+    description: string;
+    weight_kg: number;
+    height_cm: number;
+    image: string;
+}
+
+export const handleRequest: HandleRequest = async function(request: HttpRequest): Promise<HttpResponse> {
+
+    let queryParams = new URLSearchParams(decoder.decode(request.body));
+    let command: SlackCommand = {
+        text: queryParams.get("text"),
+        user_name: queryParams.get("user_name")
+    };
+    console.log(`Got a query for ${command.text}`);
+
+    if (command.text != null) {
+        let diet = await OutBoundCall(command.text);
+        if (!diet) {
+            return {
+                status: 401,
+                headers: { "content-type": "text/plain" },
+                body: "Animal not found"
+            };
+        };
+        return {
+            status: 200,
+            headers: { "content-type": "text/plain" },
+            body: `The diet for ${command.text} is ${diet}`
+        };
+    } else {
+        return {
+            status: 200,
+            headers: { "content-type": "text/plain" },
+            body: "You're not Slack!!!"
+        };
+    };
+}
+
+const OutBoundCall = async function(query: string): Promise<string | null> {
+    const animalFactResponse = await fetch(`https://freetestapi.com/api/v1/animals?search=${query}`);
+    const animalFact = await animalFactResponse.json() as AnimalFact[];
+    if (animalFact.length > 0) {
+        return animalFact[0].diet;
+    } else {
+        return null;
+    }
+};
